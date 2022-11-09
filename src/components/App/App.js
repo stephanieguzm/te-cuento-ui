af5e43fc9cc826096d9f66843fe17e6c8f4f93c1
Stephanie Guzman <sguzman247@gmail.com>
2022-11-09 16:16:09 -0600

    Add component file directory and remove boilerplate React code

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      teas: [],
+      error: ''
+    }
+  }
+
+  render() {
+    return (
+      <h1>Hello World :)</h1>
+    )
+  }
+
+}
+
+export default App;
