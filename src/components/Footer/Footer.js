900f58e5591a31fae46eba8a7aecb65a206250d1
Stephanie Guzman <sguzman247@gmail.com>
2022-11-10 17:22:04 -0600

    Add initial Footer component with cy attribute

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer-container' data-cy='footer'>
+      <p className='footer-text' data-cy='footer-text'>This is the footer space</p>
+    </footer>
+  )
+}
+
+export default Footer
