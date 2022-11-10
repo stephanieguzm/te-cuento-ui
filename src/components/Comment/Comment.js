cb841c138b400c4c5578728fe01b7cd7fa131c5d
Stephanie Guzman <sguzman247@gmail.com>
2022-11-09 21:26:59 -0600

    Render comment component to display individual comments within comments section

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Comment = ({ id, user_name, user_message }) => {
+  return (
+    <div className='comment-card' id={id}>
+      <p>{user_name}</p>
+      <p>{user_message}</p>
+    </div>
+  )
+}
+
+export default Comment
