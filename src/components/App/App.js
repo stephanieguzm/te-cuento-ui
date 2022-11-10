6c9bfe9af2d7ae998b10128bcf16316a1720632f
Stephanie Guzman <sguzman247@gmail.com>
2022-11-10 11:41:19 -0600

    Add error component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Error from '../Error/Error'
 import Header from '../Header/Header'
 import TeaContainer from '../TeaContainer/TeaContainer'
 import TeaPage from '../TeaPage/TeaPage'
@@ -13,35 +14,42 @@ class App extends Component {
     super()
     this.state = {
       teas: [],
-      error: false
+      error: ''
     }
+  }
+
+  returnHome = () => {
+    this.setState({ teas: [], error: ''})
   }
 
   componentDidMount = () => {
     getTeas()
       .then(data => this.setState({ teas: data }))
-      .catch(error => this.setState({ error: error.message }))
+      .catch(error => this.setState({ error: `${error.message}. We're busy taking care of a kettle that's boiling over! Please visit us again later.` }))
   }
 
   render() {
     return (
       <main className='App'>
         <Header />
-        <Switch>
+        {!this.state.teas.length 
+        ? <Error errorMessage={this.state.error} returnHome={this.returnHome}/>
+        : <Switch>
+          <Route 
+            exact path='/' 
+            render={() => {
+              return <TeaContainer teas={this.state.teas} returnHome={this.returnHome} />
+            }}
+          />
           <Route 
             exact path='/:id'
             render={({ match }) => {
               const teaId = match.params.id
-              return <TeaPage selectedTea={teaId}/>
+              return <TeaPage selectedTea={teaId} returnHome={this.returnHome} />
             }}
-          />
-          <Route 
-            exact path='/' 
-            render={() => {
-              return <TeaContainer teas={this.state.teas}/>
-            }}
-          />
+            />
         </Switch>
+        }
       </main>
     )
   }
