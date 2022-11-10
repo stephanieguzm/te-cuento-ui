b53350d134f7bf9501c9c67043f34261b2222626
Stephanie Guzman <sguzman247@gmail.com>
2022-11-09 21:24:38 -0600

    Add comment section and successfully fetch comment data

--- src/components/CommentsContainer/CommentsContainer.js
+++ src/components/CommentsContainer/CommentsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import CommentForm from '../CommentForm/CommentForm'
-// import CommentSection from '../CommentSection'
+import CommentSection from '../CommentSection/CommentSection'
+import { getComments } from '../../apiCalls'
 
 class CommentsContainer extends Component {
   constructor() {
@@ -8,19 +9,25 @@ class CommentsContainer extends Component {
     this.state = {
       teaComments: [],
       error: false,
-      // loading: true
+      loading: true
     }
   }
 
   componentDidMount = () => {
+    getComments()
+      .then(data => {
+        const filteredComments = data.filter(comment => comment.tea_id === parseInt(this.props.tea_id))
+        this.setState({ teaComments: filteredComments })
+        }
 
-  }
+      )}
+  
 
   render() {
     return (
       <div className='comments-container'>
-        <CommentForm tea_id={this.props.tea_id} />
-        {/* <CommentSection tea_id={this.props.tea_id} /> */}
+        <CommentForm tea_id={parseInt(this.props.tea_id)} />
+        <CommentSection teaComments={this.state.teaComments} />
       </div>
     )
   }
