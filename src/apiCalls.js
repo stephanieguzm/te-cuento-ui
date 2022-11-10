9735a60485c0827671350a44e71fb9e9031b8ff4
Stephanie Guzman <sguzman247@gmail.com>
2022-11-10 16:19:58 -0600

    Update API Error to return error status code

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ const getTeas = () => {
   return fetch('http://localhost:9000/api/v1/teas')
     .then((resp) => {
     if (!resp.ok) {
-      throw new Error(`${resp.statusCode}: Oh, no! We're busy taking care of a kettle that's boiling over! Please visit us again later.`)
+      throw new Error(resp.statusCode)
     }
     return resp.json()
   })
@@ -12,7 +12,7 @@ const getComments = () => {
   return fetch('http://localhost:9000/api/v1/comments')
     .then((resp) => {
     if (!resp.ok) {
-      throw new Error(`${resp.statusCode}: It looks like we don't have comments for this tea. Why don't you start the conversation?`)
+      throw new Error(resp.statusCode)
     }
     return resp.json()
   })
@@ -28,7 +28,7 @@ const postComment = (newComment) => {
   })
   .then((resp) => {
     if (!resp.ok) {
-      throw new Error(`${resp.statusCode}: Oops, we encountered an issue submitting your comment. Please try again.`)
+      throw new Error(resp.statusCode)
     }
     return resp.json()
   })
