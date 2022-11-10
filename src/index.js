83d1de036161f575b35650a33fb7c781fbb69873
Stephanie Guzman <sguzman247@gmail.com>
2022-11-09 19:42:25 -0600

    Remove strict mode tags

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from '../src/components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<BrowserRouter> <App /> </BrowserRouter>);
 
