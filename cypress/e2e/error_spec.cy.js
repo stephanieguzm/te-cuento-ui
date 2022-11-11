825c69291f09679ed3eb8a381c171cd99ca5320e
Stephanie Guzman <sguzman247@gmail.com>
2022-11-11 12:45:44 -0600

    Add error spec test

--- cypress/e2e/error_spec.cy.js
+++ cypress/e2e/error_spec.cy.js
@@ -2,11 +2,10 @@ describe('As a user, when I visit the application, I will see the header, a list
   it('should show a user an error message if teas are unable to be displayed on the screen', () => {
     cy.intercept('GET', 'http://localhost:9000/api/v1/teas', {
       statusCode: 404,
-      body: { message: `We're taking care of a kettle that's boiling over! Please visit us again later.` },
+      body: { message: 'Failed to Fetch' },
     })
-    cy.get('[data-cy="error-container"]').contains(`We're taking care of a kettle that's boiling over! Please visit us again later.`)
+    cy.get('[data-cy="error-container"]').contains('[data-cy="error-container"]')
   })
 
 })
 
-//  If there is an error with fetching the API information, I will get a message saying there was an error getting the tea
