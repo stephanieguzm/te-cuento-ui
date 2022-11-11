bc3f515804aeb0f4494a20aaf162e1a03e93acf0
Stephanie Guzman <sguzman247@gmail.com>
2022-11-11 09:36:23 -0600

    Add data-cy attributes to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,16 @@ import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className="header-container">
+    <header className="header-container" data-cy='header-container'>
       {/* <img src={}/> */}
       <NavLink to='/'>
-      <h1>Te Cuento</h1>
+        <h1 className='app-title' data-cy='app-title'>Te Cuento</h1>
       </NavLink>
-      <NavLink to='/'>Teas</NavLink>
+      <nav className='nav-bar' data-cy='nav-bar'>
+        <NavLink to='/'>
+          <button className='home-button'>Teas</button>
+        </NavLink>
+      </nav>
     </header>
   )
 }
