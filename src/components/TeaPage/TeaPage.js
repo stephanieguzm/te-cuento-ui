8756e374c9773c02e46ef25fee02854a0f1fb876
Stephanie Guzman <sguzman247@gmail.com>
2022-11-12 12:54:20 -0600

    Style tea info and description

--- src/components/TeaPage/TeaPage.js
+++ src/components/TeaPage/TeaPage.js
@@ -10,32 +10,39 @@ const TeaPage = ({ teas, selectedTea, returnHome, errorMessage }) => {
     <>
       {!tea
       ? <Error errorMessage={errorMessage} returnHome={returnHome} />
-      : <div className='tea-page-container' data-cy='tea-page-container'>
-          <div className='tea-info' id={tea.id} data-cy='tea-info'>
-            <h3 className='tea-p' data-cy='tea-name'>{tea.name}</h3>
-            <p className='tea-p' data-cy='tea-type'>{tea.type} tea</p>
-            <p className='tea-p' data-cy='tea-origin'>Origin: {tea.origin}</p>
-            <p className='tea-p' data-cy='tea-desc'>{tea.description}</p>
-            <p className='tea-p' data-cy='tea-brew'>
-              Steep for {tea.brew_time} minutes at {tea.temperature}°F 
-            </p>
-            <p className='tea-p' data-cy='tea-infusions'>Infusions: {tea.infusions}</p>
-            <p className='tea-p' data-cy='tea-caffeine'>
-              This tea contains a {tea.caffeine_level} level of caffeine
-            </p>
-            <img src={tea.img} alt={`${tea.name} tea`} data-cy='tea-img'/>
-            <div className='farmer-container' data-cy='farmer-container'>
-              <p className='tea-p' data-cy='farmer-name'>Farmer {tea.farmer}</p>
-              <img src={tea.farmer_img} alt={`Farmer ${tea.farmer}`} data-cy='farmer-img'/>
+      : <section className='tea-page-container' data-cy='tea-page-container'>
+          <section className='tea-header' id={tea.id} data-cy='tea-header'>
+            <img className='tea-pg-img' src={tea.img} alt={`${tea.name} tea`} data-cy='tea-img'/>
+            <div className='tea-header-details'>
+              <p className='tea-p' data-cy='tea-type'>{tea.type} tea</p>
+              <h2 className='tea-p' data-cy='tea-name'>{tea.name}</h2>
+              <p className='tea-p' data-cy='tea-origin'>Origin: {tea.origin}</p>
+              <p className='tea-p' data-cy='tea-brew'>
+                Steep for {tea.brew_time} minutes at {tea.temperature}°F 
+              </p>
+                This tea contains a {tea.caffeine_level} level of caffeine
+              <p className='tea-p' data-cy='tea-infusions'>Infusions: {tea.infusions}</p>
+              <p className='tea-p' data-cy='tea-caffeine'>
+              </p>
             </div>
-          </div>
-        <div className='comments-container' data-cy='comments-container'>
-          <CommentsContainer 
-            tea_id={selectedTea}
-            returnHome={returnHome}
-          />
-        </div>
-      </div>
+          </section>
+          <section className='desc-farmer'>
+            <div className='about'>
+              <h3>About</h3>
+              <p className='tea-p' data-cy='tea-desc'>{tea.description}</p>
+            </div>
+            <div className='farmer-container' data-cy='farmer-container'>
+              <img className='farmer-img' src={tea.farmer_img} alt={`Farmer ${tea.farmer}`} data-cy='farmer-img'/>
+              <p className='tea-p' data-cy='farmer-name'>Farmer {tea.farmer}</p>
+            </div>
+          </section>
+          <section className='comments-container' data-cy='comments-container'>
+            <CommentsContainer 
+              tea_id={selectedTea}
+              returnHome={returnHome}
+              />
+          </section>
+      </section>
       }
     </>
   )
