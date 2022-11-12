84564a79641f188d0a988a3123e7d1e896bebce3
Stephanie Guzman <sguzman247@gmail.com>
2022-11-11 19:00:25 -0600

    Add useHistory hook to push user to homepage at click

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,14 +1,19 @@
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import './Error.css'
 
 const Error = ({ errorMessage }) => {
+  const history = useHistory()
+
+  const returnHome = () => {
+    history.push('/')
+  }
+
   return (
     <section className='error-container container' data-cy='error-container'>
       <p className='error-message' data-cy='error-message'>{errorMessage}</p>
-      <Link to='/'>
-        <button className='home-button' data-cy='home-button'>Home</button>
-      </Link>
+      <button className='home-button' data-cy='home-button' onClick={returnHome}>Home</button>
+
     </section>
   )
 }
