0d5663fb33e60345cf9a46cb287376fbe5c9cd2f
Stephanie Guzman <sguzman247@gmail.com>
2022-11-13 19:00:36 -0600

    Add page not found testing spec + data-cy attribute tags

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -9,11 +9,11 @@ const PageNotFound = () => {
 
   return (
     <section className='pg-not-found-container' data-cy='pg-not-found'>
-      <h3 className='pg-not-found-message' data-cy='error-message'>You've found a page that doesn't exist. Go back home or visit these helpful resources below.</h3>
-      <button className='home-button' data-cy='home-button' onClick={goHome}>HOME</button>
-      <a className='links' href="https://journal.rishi-tea.com/what-is-tea-origins-of-camellia-sinensis/">What is Tea? Origins of Camellia Sinensis</a>
-      <a className='links' href="https://journal.rishi-tea.com/why-is-water-temperature-important-when-brewing-tea/">The Importance of Water Temperature in Tea Brewing</a>
-      <a className='links' href="https://journal.rishi-tea.com/how-to-brew-tea/">Tea Brewing Techniques</a>
+      <h3 className='pg-not-found-message' data-cy='pg-not-found-message'>You've found a page that doesn't exist. Go back home or visit these helpful resources below.</h3>
+      <button className='home-button' data-cy='not-found-home-button' onClick={goHome}>HOME</button>
+      <a className='links' data-cy='links' href="https://journal.rishi-tea.com/what-is-tea-origins-of-camellia-sinensis/">What is Tea? Origins of Camellia Sinensis</a>
+      <a className='links' data-cy='links' href="https://journal.rishi-tea.com/why-is-water-temperature-important-when-brewing-tea/">The Importance of Water Temperature in Tea Brewing</a>
+      <a className='links' data-cy='links' href="https://journal.rishi-tea.com/how-to-brew-tea/">Tea Brewing Techniques</a>
     </section>
   )
 }
